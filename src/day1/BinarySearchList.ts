567af28dbbbecf95236bca04f975b5ae01f1cfc9
Arnaud de Surirey <adesurirey@gmail.com>
2023-12-18 18:44:05 +0100

    day 1: binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,18 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0; // inclusive
+    let hi = haystack.length; // exclusive
 
+    while (lo < hi) {
+        const m = Math.floor(lo + (hi - lo) / 2);
+        const v = haystack[m];
+
+        if (needle === v) {
+            return true;
+        } else if (needle > v) {
+            lo = m + 1;
+        } else {
+            hi = m;
+        }
+    }
+    return false;
 }
